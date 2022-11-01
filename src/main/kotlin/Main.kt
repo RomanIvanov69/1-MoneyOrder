5ff75c1b45adc64ea774334ded0e9405106bf03e
Roman Ivanov <roman_ivanov85@internet.ru>
2022-11-01 19:57:10 +0300

    add fun calculation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 package ru.netology
 
 fun main() {
-    println("Hello World!")
+    val amount: Int = 1000
+    val minCommission: Int = 35
+    val commission = if (amount * 0.075 > minCommission) {
+        amount * 0.075
+    } else minCommission
+    println("При переводе: $amount рублей, комиссия составит: $commission рублей")
 }
